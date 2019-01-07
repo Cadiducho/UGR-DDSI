cdcdea2b8d43a2406732c0b7d2b6a8b6bb3a2991
Cadiducho <cadiducho@gmail.com>
2019-01-07 11:59:09 +0100

    Subir el repositorio

---
+++ src/es/ugr/ddsi/MainFestival.java
@@ -0,0 +1,48 @@
+package es.ugr.ddsi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class MainFestival {
+    
+    public static void main(String[] args) {
+        MainFestival main = new MainFestival();
+        
+        try {
+            main.initialize();
+        } catch (SQLException ex) {
+            System.err.println("Ha ocurrido un error: ");
+            System.err.println(ex.toString());
+        }
+    }
+    
+    private Connection connection;
+
+    public Connection getConnection() {
+        return connection;
+    }
+    
+    public void initialize() throws SQLException {
+        System.out.println("Estableciendo conexión...");
+        DriverManager.registerDriver( new oracle.jdbc.driver.OracleDriver());
+
+        connection = DriverManager.getConnection("jdbc:oracle:thin:@oracle0.ugr.es:1521/practbd.oracle0.ugr.es",
+            "x1957140", "x1957140");
+        connection.setAutoCommit(false);
+
+        PreparedStatement stmt = connection.prepareStatement("select 1 from edicion");
+        ResultSet rs = stmt.executeQuery();
+        if (rs.next()) {
+            if (rs.getInt(1) != 1) {
+                System.err.println("No se ha conectado correctamente a la base datos!");
+                return;
+            }
+        }
+        
+        
+    }
+    
+}
